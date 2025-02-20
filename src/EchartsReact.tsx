f59e09d34818cf4df4250ec7243b2d76e7f1b1a3
ishida <webmaster@joe1cafe.com>
2025-02-20 09:05:09 +0800

    wip

--- src/EchartsReact.tsx
+++ src/EchartsReact.tsx
@@ -1,23 +1,278 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactECharts from 'echarts-for-react'
+import './index.css'
+
+// Scatter Plot
+const scatterOption = {
+  series: [
+    {
+      type: 'scatter',
+      symbolSize: 'var(--echarts-symbol-size)',
+      itemStyle: {
+        color: 'var(--echarts-symbol-color)',
+        borderColor: 'var(--echarts-symbol-border-color)',
+        borderWidth: 'var(--echarts-symbol-border-width)',
+      },
+    },
+  ],
+}
+
+// Line Chart with Data Points
+const lineOption = {
+  series: [
+    {
+      type: 'line',
+      symbol: 'circle',
+      symbolSize: 'var(--echarts-symbol-size)',
+      showSymbol: true,
+      itemStyle: {
+        color: 'var(--echarts-symbol-color)',
+        borderColor: 'var(--echarts-symbol-border-color)',
+        borderWidth: 'var(--echarts-symbol-border-width)',
+      },
+    },
+  ],
+}
+
+// Bubble Chart
+const bubbleOption = {
+  series: [
+    {
+      type: 'scatter',
+      symbolSize: (data: number[]) => {
+        return (
+          data[2] *
+          parseFloat(
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--echarts-symbol-size'
+            )
+          )
+        )
+      },
+      itemStyle: {
+        opacity: 'var(--echarts-symbol-opacity)',
+      },
+    },
+  ],
+}
+
+// Step 7: Add event handlers
+const onEvents = {
+  click: (params: unknown) => {
+    console.log('Chart clicked:', params)
+  },
+  legendselectchanged: (params: unknown) => {
+    console.log('Legend selection changed:', params)
+  },
+}
 
 export default function Home() {
   const option = {
+    textStyle: {
+      color: 'var(--echarts-text-color)',
+    },
+    // TODO: Customize Title and Tooltip
+    title: {
+      text: 'Customized Title',
+      subtext: 'Subtitle',
+      left: 'center',
+      textStyle: {
+        color: '#0080ff',
+        fontSize: 48,
+        fontFamily: 'var(--echarts-title-font-family)',
+      },
+      subtextStyle: {
+        color: '#8000ff',
+        fontSize: 24,
+      },
+    },
+    tooltip: {
+      trigger: 'item', // 'axis' | 'item' | 'none'
+      // backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      backgroundColor: 'var(--echarts-tooltip-bg-color)',
+      borderColor: '#ccc',
+      borderWidth: 1,
+      // formatter: '{a} <br/>{b}: {c}',
+      formatter: (params: {
+        seriesName: unknown
+        name: unknown
+        value: unknown
+      }) => {
+        return `${params.seriesName}<br/>${params.name}: ${params.value}`
+      },
+    },
+    // Customize Title and Tooltip END
+    // TODO: Axis Styling
     xAxis: {
       type: 'category',
+      axisLabel: {
+        show: true,
+        // color: 'red',
+        // fontSize: 12,
+        fontFamily: 'Arial',
+        margin: 8,
+
+        color: 'var(--echarts-axis-label-color)',
+        fontSize: 'var(--echarts-axis-label-font-size)',
+        fontWeight: 'var(--echarts-axis-label-font-weight)',
+        padding: 'var(--echarts-axis-label-padding)',
+        // rotate: 'var(--echarts-axis-label-rotate)',
+      },
+      name: 'Days',
+      nameTextStyle: {
+        fontFamily: 'Arial',
+        // fontSize: 14,
+        // fontWeight: 'bold',
+        // color: '#300ff3',
+
+        color: 'var(--echarts-axis-title-color)',
+        fontSize: 'var(--echarts-axis-title-font-size)',
+        fontWeight: 'var(--echarts-axis-title-font-weight)',
+        padding: 'var(--echarts-axis-title-padding)',
+      },
       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: 'var(--echarts-axis-line-color)',
+          width: 'var(--echarts-axis-line-width)',
+          type: 'var(--echarts-axis-line-type)',
+        },
+      },
+
+      axisTick: {
+        show: true,
+        length: 'var(--echarts-axis-tick-length)',
+        lineStyle: {
+          width: 'var(--echarts-axis-tick-width)',
+          color: 'var(--echarts-axis-tick-color)',
+        },
+      },
+
+      nameLocation: 'middle',
+      nameGap: 30,
+      splitLine: {
+        show: false,
+        lineStyle: {
+          color: 'var(--echarts-split-line-color)',
+          type: 'dashed',
+        },
+      },
     },
     yAxis: {
       type: 'value',
+      name: 'Values',
+      nameTextStyle: {
+        fontSize: 14,
+        color: '#333',
+      },
+      axisLabel: {
+        color: '#666',
+        fontSize: 12,
+      },
     },
+    // Axis Styling END
+    // TODO: Customize Legend
+    legend: {
+      data: ['Series 1', 'Series 2'],
+      orient: 'horizontal',
+      left: 'right',
+      show: true,
+      type: 'scroll',
+      bottom: '5%',
+      textStyle: {
+        color: 'var(--echarts-legend-text-color)',
+        fontSize: 'var(--echarts-legend-font-size)',
+      },
+      itemStyle: {
+        borderColor: 'var(--echarts-legend-border-color)',
+        borderWidth: 1,
+      },
+      emphasis: {
+        textStyle: {
+          color: 'var(--echarts-legend-emphasis-color)',
+        },
+      },
+    },
+    // Customize Legend END
+    // TODO: Customize Grid
+    grid: {
+      top: '15%',
+      right: '5%',
+      bottom: '15%',
+      left: '5%',
+      containLabel: true,
+      borderColor: 'var(--echarts-grid-border-color)',
+      borderWidth: 1,
+      backgroundColor: 'var(--echarts-grid-bg-color)',
+    },
+    // Customize Grid END
     series: [
       {
+        name: 'Series 1',
         data: [150, 230, 224, 218, 135, 147, 260],
         type: 'line',
+        // TODO: Customize Series Style
+        itemStyle: {
+          // color: '#5470c6', // TODO: Color of the data points
+          borderColor: '#333', // TODO: Series Border color
+          borderWidth: 2, // TODO: Series Border width
+          opacity: 0.8, // TODO: Series Opacity
+        },
+        label: {
+          show: true, // TODO: Show labels
+          color: '#fff', // TODO: Label color
+          fontSize: 12, // TODO: Label font size
+          fontFamily: 'Arial', // TODO: Label font family
+        },
+        // Customize Series Style END
+        symbol: 'circle', // TODO: Shape of the data points
+        symbolSize: 10, // TODO: Size of the data points
+        emphasis: {
+          scale: 'var(--echarts-emphasis-scale)',
+          itemStyle: {
+            shadowBlur: 'var(--echarts-emphasis-shadow-blur)',
+            shadowColor: 'var(--echarts-emphasis-shadow-color)',
+          },
+        },
+        select: {
+          itemStyle: {
+            color: 'var(--echarts-symbol-selected-color)',
+            opacity: 'var(--echarts-symbol-selected-opacity)',
+          },
+        },
+      },
+      {
+        name: 'Series 2',
+        type: 'bar',
+        data: [150, 230, 224, 218, 135, 147, 260],
       },
     ],
   }
 
-  return <ReactECharts option={option} style={{ height: '500px' }} />
+  const chartRef = useRef(null)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (chartRef.current) {
+        chartRef.current.getEchartsInstance().resize()
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <ReactECharts
+      ref={chartRef}
+      option={option}
+      style={{ height: '500px' }}
+      onEvents={onEvents}
+      notMerge={true}
+      lazyUpdate={true}
+    />
+  )
 }
---
+++ src/Index.tsx
@@ -0,0 +1,137 @@
+import EchartsDemo from './EchartsDemo'
+import EchartsDemo2 from './EchartsDemo2'
+import EchartsReact from './EchartsReact'
+import EchartsOne from './EchartsOne'
+import MyChart from './MyChart'
+
+function Index() {
+
+  return (
+    <div className="App">
+      <EchartsDemo />
+      <EchartsDemo2 />
+      <EchartsReact />
+      <EchartsOne />
+      <MyChart />
+    </div>
+  )
+}
+
+export default Index
+
+// FIXME: Color
+// --echarts-bg-color               /* Background color */
+// --echarts-text-color            /* Text color */
+// --echarts-axis-line-color       /* Axis line color */
+// --echarts-axis-tick-color       /* Axis tick mark color */
+// --echarts-axis-label-color      /* Axis label color */
+// --echarts-grid-line-color       /* Grid line color */
+// --echarts-legend-text-color     /* Legend text color */
+// --echarts-tooltip-bg-color      /* Tooltip background color */
+// --echarts-tooltip-border-color  /* Tooltip border color */
+// --echarts-tooltip-text-color    /* Tooltip text color */
+// --echarts-series-colors         /* Series colors (comma-separated list) */
+
+// FIXME: Size and spacing 
+// --echarts-font-size            /* Base font size */
+// --echarts-axis-label-size      /* Axis label font size */
+// --echarts-legend-font-size     /* Legend font size */
+// --echarts-tooltip-font-size    /* Tooltip font size */
+// --echarts-line-width           /* Line width for axes and grid */
+// --echarts-point-size           /* Data point size */
+// --echarts-padding              /* Chart padding */
+
+// FIXME: Font
+// --echarts-font-family         /* Base font family */
+// --echarts-title-font-family   /* Title font family */
+// --echarts-label-font-family   /* Label font family */
+
+
+// FIXME: Color Variables
+// --echarts-color-1
+// --echarts-color-2
+// --echarts-color-3
+// --echarts-color-4
+// --echarts-color-5
+// --echarts-color-6
+// --echarts-color-7
+// --echarts-color-8
+// --echarts-color-9
+// --echarts-color-10
+// FIXME: Font Variables
+// --echarts-font-family
+// --echarts-font-size
+// --echarts-font-weight
+// --echarts-font-color
+// FIXME: Background Variables
+// --echarts-background-color
+// --echarts-grid-background-color
+// FIXME: Border Variables
+// --echarts-border-color
+// --echarts-border-width
+// --echarts-border-radius
+// FIXME: Shadow Variables
+// --echarts-shadow-color
+// --echarts-shadow-blur
+// --echarts-shadow-offset-x
+// --echarts-shadow-offset-y
+// FIXME: Line Style Variables
+// --echarts-line-width
+// --echarts-line-color
+// --echarts-line-type
+// FIXME: Tooltip Variables
+// --echarts-tooltip-background
+// --echarts-tooltip-border-radius
+// --echarts-tooltip-font-color
+// FIXME: Legend Variables
+// --echarts-legend-item-color
+// --echarts-legend-font-size
+
+
+/* FIXME: Axis Title Variables */
+// --echarts-axis-title-color        /* Color of axis titles */
+// --echarts-axis-title-font-size    /* Font size of axis titles */
+// --echarts-axis-title-font-weight  /* Font weight of axis titles */
+// --echarts-axis-title-padding      /* Padding around axis titles */
+
+/* FIXME: Axis Label Variables */
+// --echarts-axis-label-color        /* Color of axis labels */
+// --echarts-axis-label-font-size    /* Font size of axis labels */
+// --echarts-axis-label-font-weight  /* Font weight of axis labels */
+// --echarts-axis-label-padding      /* Padding around axis labels */
+// --echarts-axis-label-rotate       /* Rotation angle for axis labels */
+// --echarts-axis-label-margin       /* Margin between labels and axis */
+// --echarts-axis-label-font-family
+
+/* FIXME: Axis Line Variables */
+// --echarts-axis-line-width         /* Width of axis lines */
+// --echarts-axis-line-color         /* Color of axis lines */
+// --echarts-axis-line-type          /* Type of axis lines (solid, dashed, etc) */
+
+/* FIXME: Axis Tick Variables */
+// --echarts-axis-tick-length        /* Length of axis ticks */
+// --echarts-axis-tick-width         /* Width of axis ticks */
+// --echarts-axis-tick-color         /* Color of axis ticks */
+
+/* FIXME: Symbol/Point Basic Styling */
+// --echarts-symbol-size            /* Size of data points */
+// --echarts-symbol-color           /* Color of data points */
+// --echarts-symbol-border-color    /* Border color of data points */
+// --echarts-symbol-border-width    /* Border width of data points */
+// --echarts-symbol-opacity         /* Opacity of data points */
+
+/* FIXME: Symbol/Point Hover State */
+// --echarts-symbol-hover-size      /* Size of data points on hover */
+// --echarts-symbol-hover-color     /* Color of data points on hover */
+// --echarts-symbol-hover-opacity   /* Opacity of data points on hover */
+
+/* FIXME: Symbol/Point Selected State */
+// --echarts-symbol-selected-size    /* Size of selected data points */
+// --echarts-symbol-selected-color   /* Color of selected data points */
+// --echarts-symbol-selected-opacity /* Opacity of selected data points */
+
+/* FIXME: Symbol/Point Emphasis */
+// --echarts-emphasis-scale         /* Scale factor for emphasized points */
+// --echarts-emphasis-blur          /* Blur amount for non-emphasized points */
+// --echarts-emphasis-shadow-blur   /* Shadow blur for emphasized points */
+// --echarts-emphasis-shadow-color  /* Shadow color for emphasized points */
